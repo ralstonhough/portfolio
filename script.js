6b3ae979747ff77e436492b2875a76d00bfaecb4
Ralston Hough <ralstonhough@gmail.com>
2022-11-19 00:08:09 -0500

    Nice interaction

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 let heroImage = document.getElementById("heroImage");
 let contacts = document.getElementsByClassName("contact");
 let menu = document.getElementById("menu");
+let unselected = document.getElementsByClassName("unselected");
+
+for (let target of unselected) {
+    target.onmouseover = function(){
+        this.style.textDecoration = "underline";
+        this.style.textUnderlineOffset = "8px";
+    };
+    target.onmouseleave = function(){
+        this.style.textDecoration = "none";
+    };
+};
 
 heroImage.onmouseover = function(){
     this.style.filter = "grayscale(0%)";
