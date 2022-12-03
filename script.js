f369b4177436d35de33c0a964b996a14e1e13194
Ralston Hough <ralstonhough@gmail.com>
2022-12-02 20:11:33 -0500

    Lots of stuff, big day

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
-let heroImage = document.getElementById("heroImage");
-let contacts = document.getElementsByClassName("contact");
 let menu = document.getElementById("menu");
-let menuBars = document.getElementsByClassName("menuBar");
-let unselected = document.getElementsByClassName("unselected");
 let dropDown = document.getElementById("dropDown");
 let content = document.getElementById("content");
 let dropNavCats = document.getElementsByClassName("dropNavCat");
 let dropDownOpacity = 0;
-
-for (let target of unselected) {
-    target.onmouseover = function(){
-        this.style.textDecoration = "underline";
-        this.style.textUnderlineOffset = "8px";
-    };
-    target.onmouseleave = function(){
-        this.style.textDecoration = "none";
-    };
-};
-
-menu.onmouseover = function(){
-    this.style.transform = "scale(1.1,1.1)";
-};
-
-menu.onmouseleave = function(){
-    this.style.transform = "scale(1,1)";
-};
 
 menu.onclick = function(){
     if (dropDownOpacity == 0) {
@@ -51,15 +29,6 @@ menu.onclick = function(){
             }, 300);
         };
         dropDownOpacity = 0;
-    };
-};
-
-for (let contact of contacts) {
-    contact.onmouseover = function(){
-        this.style.transform = "scale(1.1,1.1)";
-    };
-    contact.onmouseleave = function(){
-        this.style.transform = "scale(1,1)";
     };
 };
 
